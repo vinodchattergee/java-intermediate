4c7bae8fc6bb86529ef5fde83e7892884a08123c
vinod <vinod.chattergee@tesco.com>
2022-05-23 20:54:43 +0530

    Added gitignore

---
+++ src/com/intermediate/PackagesApp.java
@@ -0,0 +1,10 @@
+package com.intermediate;
+
+import java.util.Random;
+
+public class PackagesApp {
+    public static void main(String[] args) {
+        Random randomNumber=new Random();
+        System.out.println( randomNumber.nextInt(99));
+    }
+}
